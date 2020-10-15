1cc93706747d75fc3608a70ae9f5276128ab116a
Miguel <miguelaugl@outlook.com>
2020-10-14 23:51:52 -0300

    splitting into two folders

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
+import { FaArrowRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import '../styles/pages/landing.css';
@@ -23,7 +23,7 @@ function Landing() {
         </div>
 
         <Link to="/app" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+          <FaArrowRight size={26} />
         </Link>
       </div>
     </div>
